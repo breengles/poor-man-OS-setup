c4bc7dbdeb552fa3e01e8e33ab19f1fb4934cc6d
Artem Kotov <breengles@gmail.com>
2023-11-01 19:35:27 +0400

    upd

--- restic/restic-backup.sh
+++ restic/restic-backup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-# Runs Restic backup on a schedule via (ana)cron
-# put it in /etc/cron.daily
+# Runs Restic backup on a schedule via cron, emails with status
+# 1. add a cred file with your repo logins to /etc/restic/cred
+# 2. 
 
-LOG="${HOME}/.log/restic.log"
+LOG="/home/artem/.log/restic.log"
 
-export RESTIC_REPOSITORY="/path/to/repo"
-export RESTIC_PASSWORD_FILE="/path/to/restic-password.txt"
+backup_disk="/media/artem/BREE_STOR"
+export RESTIC_REPOSITORY="$backup_disk/restic-backups"
+export RESTIC_PASSWORD_FILE="/home/artem/restic-password.txt"
 
 ### keep last # of days of snapshots
 KEEPDAYS=30
 
 log() { 
-    echo -e "$(date "+%Y%m%d_%H%M%S"): ${1}" | tee -a "$LOG"
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S"): ${1}" | tee -a "$LOG"
 }
 
 echo -e "\n" | tee -a "$LOG"
+
+if ! mountpoint -q $backup_disk; then
+  log "$backup_disk is not mounted"
+  exit 1
+fi
 
 log "starting backup.."
 msg=$(restic --exclude=".git" --verbose backup projects Sync >> "$LOG" 2>&1)
