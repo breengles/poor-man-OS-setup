c9ad6a491306b2d6a12f044adf3e4b3f4c7f4018
Artem Kotov <breengles@gmail.com>
2024-07-22 14:46:19 +0400

    upd colorscheme plug for nvim

--- dotconfig/nvim/lua/plugins/colorscheme.lua
+++ dotconfig/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
     -- add gruvbox
-    { "mycolorscheme.vim" },
+    {
+        "mycolorscheme.vim",
+        name="mycolorscheme_plg",
+        dir="colors/mycolorscheme.vim",
+        dev=true,
+    },
 
     -- Configure LazyVim to load gruvbox
     {
