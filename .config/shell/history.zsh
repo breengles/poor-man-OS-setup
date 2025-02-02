3809d499cc1650250d8c90285b2719c1a98eac5e
Artem Kotov <breengles@gmail.com>
2025-02-03 02:12:17 +0300

    move completions from history

--- .config/shell/history.zsh
+++ .config/shell/history.zsh
@@ -9,10 +9,3 @@ setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_ignore_dups
 setopt hist_find_no_dups
-
-# Completion styling
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color=always $realpath'
