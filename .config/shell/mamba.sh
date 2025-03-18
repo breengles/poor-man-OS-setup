9d91ea19677c644a38a84202e8c7051a8090274c
Artem Kotov <breengles@gmail.com>
2025-03-18 14:33:40 +0300

    use mamba's specific module for init

---
+++ .config/shell/mamba.sh
@@ -0,0 +1,9 @@
+export MAMBA_EXE="$HOME/miniforge3/bin/mamba";
+export MAMBA_ROOT_PREFIX="$HOME/miniforge3";
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
