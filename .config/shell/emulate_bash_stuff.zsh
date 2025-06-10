cf6a562509285e10474a936ef1bdc5b136293f95
Artem Kotov <breengles@gmail.com>
2025-06-10 18:21:35 +0200

    remove redundant comment

--- .config/shell/emulate_bash_stuff.zsh
+++ .config/shell/emulate_bash_stuff.zsh
@@ -1,4 +1,3 @@
-# if running from bash as login shell on remote cluster - uncomment the following lines
 if [[ $(hostname) == "lambda-loginnode"* ]]; then
     emulate sh -c "source /etc/profile"
     module load slurm
