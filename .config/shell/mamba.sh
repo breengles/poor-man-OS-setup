32ccc8eb2affd6befcd7a7e5086d7e124c1ba03a
Artem Kotov <breengles@gmail.com>
2025-10-24 23:04:49 +0200

    Refactor functions.sh and mamba.sh: remove mambac function from functions.sh and add it to mamba.sh, streamline update function by removing mamba update command, and clean up zinit.zsh by removing conda-zsh-completion.

--- .config/shell/mamba.sh
+++ .config/shell/mamba.sh
@@ -27,3 +27,49 @@ if [ -d "$HOME/miniforge3" ]; then
     fi
     unset __mamba_setup
 fi
+
+function mambac {
+  local python_version
+  local env_name
+
+  python_version="3.11"
+  env_name="$(basename "$(pwd)")"
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -p)
+        shift
+        if [[ $# -gt 0 ]]; then
+          python_version="$1"
+          shift
+        else
+          echo "Error: Python version argument missing after -p"
+          return 1
+        fi
+        ;;
+      -n)
+        shift
+        if [[ $# -gt 0 ]]; then
+          env_name="$1"
+          shift
+        else
+          echo "Error: Environment name argument missing after -n"
+          return 1
+        fi
+        ;;
+      *)
+        echo "Usage: mambac [-p python_version] [-n env_name]"
+        return 1
+        ;;
+    esac
+  done
+
+  echo "Creating environment \`$env_name\` with python \`$python_version\` ..."
+  if conda env list | grep -q "^$env_name\b"; then
+    echo "Environment '$env_name' already exists."
+  else
+    mamba create -y -n "$env_name" python="$python_version"
+  fi
+
+  mamba activate "$env_name"
+}
