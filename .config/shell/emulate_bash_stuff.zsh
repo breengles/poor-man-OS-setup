06231dd81b25cc67b8dffac83014c420348c525b
Artem Kotov <breengles@gmail.com>
2025-10-30 10:06:49 +0100

    Add environment variable exports for Hugging Face in emulate_bash_stuff.zsh

--- .config/shell/emulate_bash_stuff.zsh
+++ .config/shell/emulate_bash_stuff.zsh
@@ -2,4 +2,6 @@ if [[ $(hostname) == "lambda-loginnode"* ]]; then
     emulate sh -c "source /etc/profile"
     module load slurm
     module load cuda12.4/toolkit/12.4.1
+    export HF_HOME="/weka/teams/gte/huggingface/hub"
+    export HF_HUB_CACHE="$HF_HOME"
 fi
