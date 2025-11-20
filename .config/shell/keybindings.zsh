00f8e53b3c8839bc7d4acd34cd38505737d21509
Artem Kotov <breengles@gmail.com>
2025-11-20 11:39:46 +0100

    Add key bindings for word and line navigation in .config/shell/keybindings.zsh to enhance text editing efficiency.

--- .config/shell/keybindings.zsh
+++ .config/shell/keybindings.zsh
@@ -6,3 +6,11 @@ zle -N down-line-or-beginning-search
 
 bindkey "^[[A" up-line-or-beginning-search # Up
 bindkey "^[[B" down-line-or-beginning-search # Down
+
+# Word navigation (Alt+Left, Alt+Right)
+bindkey "^[b" backward-word # Alt+Left (from kitty mapping)
+bindkey "^[f" forward-word # Alt+Right (from kitty mapping)
+
+# Line navigation (Cmd+Left, Cmd+Right)
+bindkey "^A" beginning-of-line # Cmd+Left (from kitty mapping)
+bindkey "^E" end-of-line # Cmd+Right (from kitty mapping)
