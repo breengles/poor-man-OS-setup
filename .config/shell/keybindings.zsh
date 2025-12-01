31b74ef06474cb8c37964d2b80ea445d8cdc70f0
Artem Kotov <breengles@gmail.com>
2025-12-01 17:55:02 +0100

    Add xterm-keys support in .tmux.conf and enhance keybindings in keybindings.zsh for improved text editing

--- .config/shell/keybindings.zsh
+++ .config/shell/keybindings.zsh
@@ -11,6 +11,12 @@ bindkey "^[[B" down-line-or-beginning-search # Down
 bindkey "^[b" backward-word # Alt+Left (from kitty mapping)
 bindkey "^[f" forward-word # Alt+Right (from kitty mapping)
 
+# Word deletion (Alt+Backspace)
+bindkey "^[^?" backward-kill-word # Alt+Backspace
+
 # Line navigation (Cmd+Left, Cmd+Right)
 bindkey "^A" beginning-of-line # Cmd+Left (from kitty mapping)
 bindkey "^E" end-of-line # Cmd+Right (from kitty mapping)
+
+# Line deletion (Cmd+Backspace)
+bindkey "^U" kill-whole-line # Cmd+Backspace
