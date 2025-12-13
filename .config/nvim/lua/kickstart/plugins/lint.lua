4127e7b18f0c72de19549b2b3863093ea55baffe
Artem Kotov <breengles@gmail.com>
2025-12-13 16:54:16 +0100

    update

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -7,6 +7,8 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        -- Note: ruff LSP already provides linting, this is a fallback
+        python = { 'ruff' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
